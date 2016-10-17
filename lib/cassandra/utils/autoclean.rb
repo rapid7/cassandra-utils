0516c800ee4af3123a3bd5d221ced63ae4aa7294
Frank Mitchell <me@frankmitchell.org>
2016-10-17 15:12:46 -0400

    Call "nodetool ring" and parse the resutls for tokens.

--- lib/cassandra/utils/autoclean.rb
+++ lib/cassandra/utils/autoclean.rb
@@ -6,6 +6,25 @@ module Cassandra
      def run!
      end
 
+     # Get the tokens this node owns
+     #
+     # The "nodetool ring" command returns
+     #
+     # Address    Rack  Status  State   Load  Size  Owns  Token
+     # 127.0.0.1  r1    Up      Normal  10    GB    33%   123456789
+     #
+     # @return [Array<String>] Tokens owned by this node
+     #
+     def tokens
+       return [] if address.nil?
+       results = (nodetool_ring || '').split("\n")
+       results.map! { |line| line.strip }
+       results.select! { |line| line.start_with? address }
+       results.map! { |line| line.split(/\s+/)[7] }
+       results.compact!
+       results.sort
+     end
+
      # Get the IP address of this node
      #
      # @return [String, nil] IP address of this node
@@ -16,6 +35,22 @@ module Cassandra
          @address = addr.ip_address unless addr.nil?
        end
        @address
+     end
+
+     private
+
+     # Run the "nodetool ring" command and return the output
+     #
+     # @return [String, nil] Output from the "nodetool ring" command
+     #
+     def nodetool_ring
+       @nodetool_ring ||= Mixlib::ShellOut.new('nodetool ring', {
+         :cwd => '/tmp',
+         :timeout => 120
+       })
+       @nodetool_ring.run_command
+       @nodetool_ring.error!
+       @nodetool_ring.stdout
      end
    end
   end
--- test/cassandra/autoclean_test.rb
+++ test/cassandra/autoclean_test.rb
@@ -4,6 +4,20 @@ require 'test_helper'
 # https://github.com/logstash-plugins/logstash-patterns-core/blob/v4.0.2/patterns/grok-patterns#L30
 IPV4 = Regexp.new('(?<![0-9])(?:(?:[0-1]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])[.](?:[0-1]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])[.](?:[0-1]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])[.](?:[0-1]?[0-9]{1,2}|2[0-4][0-9]|25[0-5]))(?![0-9])')
 
+class MockShellOut
+  attr_reader :stdout
+
+  def initialize(stdout)
+    @stdout = stdout
+  end
+
+  def run_command
+  end
+
+  def error!
+  end
+end
+
 describe Cassandra::Utils::Autoclean do
   before do
     @cleaner = Cassandra::Utils::Autoclean.new
@@ -39,6 +53,43 @@ describe Cassandra::Utils::Autoclean do
 
       Socket.stub :ip_address_list, addresses do
         @cleaner.address.must_equal '10.0.0.1'
+      end
+    end
+  end
+
+  describe :tokens do
+    it 'returns tokens owned by this node' do
+      shellout = lambda do |command, options|
+        command.must_equal 'nodetool ring'
+
+        results = <<-END
+        Note: Ownership information does not include topology; for complete information, specify a keyspace
+
+        Datacenter: dc1
+        ==========
+        Address       Rack        Status State   Load            Owns                Token
+                                                                                     orphan-token
+        10.0.0.1      r1          Up     Normal  1 GB            33%                 2
+        10.0.0.1      r1          Up     Normal  1 GB            33%                 1
+        10.0.0.3      r3          Up     Joining 1 GB            ?                   5
+        10.0.0.2      r2          Down   Normal  1 GB            33%                 4
+        10.0.0.1      r1          Up     Normal  1 GB            33%                 3
+
+          Warning: "nodetool ring" is used to output all the tokens of a node.
+          To view status related info of a node use "nodetool status" instead.
+        END
+
+        MockShellOut.new(results)
+      end
+
+      addresses = lambda do
+        [Addrinfo.tcp('10.0.0.1', 0)]
+      end
+
+      Mixlib::ShellOut.stub :new, shellout do
+        Socket.stub :ip_address_list, addresses do
+          @cleaner.tokens.must_equal ['1', '2', '3']
+        end
       end
     end
   end
