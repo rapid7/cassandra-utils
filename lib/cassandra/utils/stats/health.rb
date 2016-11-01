19ca868d96a33a9bfcb01e90397480d19afd7bda
Frank Mitchell <me@frankmitchell.org>
2016-11-01 09:25:00 -0400

    Test that the node state is normal when checking running status.

--- lib/cassandra/utils/stats/health.rb
+++ lib/cassandra/utils/stats/health.rb
@@ -4,13 +4,32 @@ module Cassandra
       class Health < Utils::CLI::Base
         def run!
           running = true
-          running &= nodetool_statusgossip.strip.include?('running')
-          running &= nodetool_statusthrift.strip.include?('running')
-          to_dd(running)
+          running &= nodetool_statusgossip.strip == 'running'
+          running &= nodetool_statusthrift.strip == 'running'
+          running &= state == :normal
+          running = to_dd running
+          push_metric running
+          running
         end
 
         def metric_name
           'cassandra.service.running'
+        end
+
+        # Return the state of the Cassandra node
+        #
+        # The returned state is reported by "nodetool netstats".
+        #
+        # @return [state, nil]
+        #
+        def state
+          results = (nodetool_netstats || '').split("\n")
+          results.map! { |line| line.strip }
+          results.select! { |line| line.include? 'Mode:' }
+          results.map! { |line| line.split(':')[1] }
+          results.compact!
+          return nil if results.size != 1
+          results.first.strip.downcase.to_sym
         end
 
         private
@@ -33,6 +52,16 @@ module Cassandra
           @nodetool_statusthrift||= DaemonRunner::ShellOut.new(command: 'nodetool statusthrift')
           @nodetool_statusthrift.run!
           @nodetool_statusthrift.stdout
+        end
+
+        # Run the "nodetool netstats' command and return the output
+        #
+        # @return [String, nil] Output from the "nodetool netstats" command
+        #
+        def nodetool_netstats
+          @nodetool_netstats ||= DaemonRunner::ShellOut.new(command: 'nodetool netstats', timeout: 120)
+          @nodetool_netstats.run!
+          @nodetool_netstats.stdout
         end
       end
     end
---
+++ test/cassandra/health_check_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+describe Cassandra::Utils::Stats::Health do
+  before do
+    @checker = Cassandra::Utils::Stats::Health.new
+  end
+
+  describe :run! do
+    it 'succeeds if node is normal and thrift and gossip are running' do
+      @checker.stub :nodetool_netstats, 'Mode: NORMAL' do
+        @checker.stub :nodetool_statusthrift, 'running' do
+          @checker.stub :nodetool_statusgossip, 'running' do
+            @checker.run!.must_equal 1
+          end
+        end
+      end
+    end
+
+    it 'fails if gossip is down' do
+      @checker.stub :nodetool_netstats, 'Mode: NORMAL' do
+        @checker.stub :nodetool_statusgossip, 'down' do
+          @checker.stub :nodetool_statusthrift, 'running' do
+            @checker.run!.must_equal 0
+          end
+        end
+      end
+    end
+
+    it 'fails if thrift is down' do
+      @checker.stub :nodetool_netstats, 'Mode: NORMAL' do
+        @checker.stub :nodetool_statusthrift, 'down' do
+          @checker.stub :nodetool_statusgossip, 'running' do
+            @checker.run!.must_equal 0
+          end
+        end
+      end
+    end
+  end
+end
