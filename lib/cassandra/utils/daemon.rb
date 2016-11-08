41c9b93b54fb3882749666b4732cb3efc00ce047
Andrew Thompson <Andrew_Thompson@rapid7.com>
2016-11-08 16:10:32 -0500

    Add Cleanup task
    The cleanup task should _cache_ the instance of the class.

--- lib/cassandra/utils/daemon.rb
+++ lib/cassandra/utils/daemon.rb
@@ -6,10 +6,17 @@ module Cassandra
 
       def tasks
         [
+          [auto_clean_task, 'run!'],
           [::Cassandra::Utils::Stats::Health.new, 'run!'],
           [::Cassandra::Utils::Stats::Compaction.new, 'run!'],
           [::Cassandra::Utils::Stats::Cleanup.new, 'run!']
         ]
+      end
+
+      private
+
+      def auto_clean_task
+        @auto_clean_task ||= ::Cassandra::Tasks::Autoclean.new(options)
       end
     end
   end
