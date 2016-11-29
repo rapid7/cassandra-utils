3f80b23169e788cd53a1ca510e215098c8497949
Frank Mitchell <me@frankmitchell.org>
2016-11-29 15:35:29 -0500

    Read the data center from "nodetool info".

--- lib/cassandra/tasks/seedregistry.rb
+++ lib/cassandra/tasks/seedregistry.rb
@@ -1,3 +1,5 @@
+require 'daemon_runner/shell_out'
+
 module Cassandra
   module Tasks
    class SeedRegistry
@@ -10,6 +12,28 @@ module Cassandra
      def initialize cluster_name
        @cluster_name = cluster_name.to_s
        raise ArgumentError.new('cluster_name must not be empty') if @cluster_name.empty?
+     end
+
+     def data_center
+       results = (nodetool_info || '').split("\n")
+       results.map! { |line| line.strip }
+       results.select! { |line| line.include?('Data Center') }
+       results.map! { |line| line.split(':')[1] }
+       results.compact!
+       return nil if results.size != 1
+       results.first.strip
+     end
+
+     private
+
+     # Run the "nodetool info" command and return the output
+     #
+     # @return [String, nil] Output from the "nodetool info" command
+     #
+     def nodetool_info
+       @nodetool_info ||= DaemonRunner::ShellOut.new(command: 'nodetool info', timeout: 300)
+       @nodetool_info.run!
+       @nodetool_info.stdout
      end
    end
   end
--- test/cassandra/seedregistry_test.rb
+++ test/cassandra/seedregistry_test.rb
@@ -1,5 +1,20 @@
 require 'test_helper'
 
+class MockShellOut
+  attr_reader :stdout
+  attr_accessor :valid_exit_codes
+
+  def initialize(stdout)
+    @stdout = stdout
+  end
+
+  def run_command
+  end
+
+  def error!
+  end
+end
+
 describe Cassandra::Tasks::SeedRegistry do
   describe :new do
     it 'requires a non-nil cluster name' do
@@ -8,6 +23,40 @@ describe Cassandra::Tasks::SeedRegistry do
 
     it 'requires a non-empty cluster name' do
       proc { Cassandra::Tasks::SeedRegistry.new('') }.must_raise ArgumentError
+    end
+  end
+
+  describe :data_center do
+    it 'returns the data center for this node' do
+      shellout = lambda do |command, options|
+        command.must_equal 'nodetool info'
+
+        results = <<-END
+        Token                  : (invoke with -T/--tokens to see all 256 tokens)
+        ID                     : uuid
+        Gossip active          : true
+        Thrift active          : true
+        Native Transport active: true
+        Load                   : 0.0 GB
+        Generation No          : 0
+        Uptime (seconds)       : 0
+        Heap Memory (MB)       : 0 / 0
+        Off Heap Memory (MB)   : 0
+        Data Center            : us-east
+        Rack                   : 1d
+        Exceptions             : 0
+        Key Cache              : size 0 (bytes), capacity 0 (bytes), 0 hits, 0 requests, NaN recent hit rate, 0 save period in seconds
+        Row Cache              : size 0 (bytes), capacity 0 (bytes), 0 hits, 0 requests, NaN recent hit rate, 0 save period in seconds
+        END
+
+        MockShellOut.new(results)
+      end
+
+      registry = Cassandra::Tasks::SeedRegistry.new('test')
+
+      Mixlib::ShellOut.stub :new, shellout do
+        registry.data_center.must_equal 'us-east'
+      end
     end
   end
 end
