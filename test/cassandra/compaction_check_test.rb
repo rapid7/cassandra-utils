3fb339e90e52d0b28c155ada240463533ef2bab6
Frank Mitchell <me@frankmitchell.org>
2016-11-30 13:11:47 -0500

    Provide tests for compaction stats reporting.

---
+++ test/cassandra/compaction_check_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class MockShellOut
+  attr_reader :stdout
+  attr_accessor :valid_exit_codes
+
+  def initialize(stdout)
+    @stdout = stdout
+  end
+
+  def run_command
+  end
+
+  def error!
+  end
+end
+
+describe Cassandra::Utils::Stats::Compaction do
+  before do
+    @compactor = Cassandra::Utils::Stats::Compaction.new
+  end
+
+  it 'succeeds if compaction is running' do
+    shellout = lambda do |command, options|
+      command.must_equal 'nodetool compactionstats'
+
+      results = <<-END
+      pending tasks: 1
+                compaction type  keyspace  table  completed  total  unit  progress
+                      Compaction test      test   3          100    bytes    3.14%
+      Active compaction remaining time : 0h03m14s
+      END
+
+      MockShellOut.new(results)
+    end
+
+    Mixlib::ShellOut.stub :new, shellout do
+      @compactor.run!.must_equal true
+    end
+  end
+
+  it 'fails if compaction is not running' do
+    shellout = lambda do |command, options|
+      command.must_equal 'nodetool compactionstats'
+
+      results = <<-END
+      pending tasks: 0
+      Active compaction remaining time : n/a
+      END
+
+      MockShellOut.new(results)
+    end
+
+    Mixlib::ShellOut.stub :new, shellout do
+      @compactor.run!.must_equal false
+    end
+  end
+end
