26e75b2c30d02a568d0b1608c8a618ca4d51db28
Frank Mitchell <me@frankmitchell.org>
2016-11-30 11:48:49 -0500

    Provide a more accurate cleanup test.

--- test/cassandra/cleanup_check_test.rb
+++ test/cassandra/cleanup_check_test.rb
@@ -1,11 +1,41 @@
 require 'test_helper'
 
+class MockShellOut
+  attr_reader :stdout
+  attr_accessor :valid_exit_codes
+
+  def initialize(stdout)
+    @stdout = stdout
+  end
+
+  def run_command
+  end
+
+  def error!
+  end
+end
+
 describe Cassandra::Utils::Stats::Cleanup do
   before do
     @cleanup = Cassandra::Utils::Stats::Cleanup.new
   end
 
   it 'succeeds if cleanup is running' do
-    @cleanup.run!.must_equal true
+    shellout = lambda do |command, options|
+      command.must_equal 'nodetool compactionstats'
+
+      results = <<-END
+      pending tasks: 1
+                compaction type  keyspace  table  completed  total  unit  progress
+                        Cleanup  test      test   3          100    bytes    3.14%
+      Active compaction remaining time : 0h03m14s
+      END
+
+      MockShellOut.new(results)
+    end
+
+    Mixlib::ShellOut.stub :new, shellout do
+      @cleanup.run!.must_equal true
+    end
   end
 end
