51f744f17ac58c37822f1829e07fe71bce1aac03
Frank Mitchell <me@frankmitchell.org>
2016-11-30 13:15:02 -0500

    Move the MockShellOut class into its own file.

--- test/cassandra/cleanup_check_test.rb
+++ test/cassandra/cleanup_check_test.rb
@@ -1,19 +1,5 @@
 require 'test_helper'
-
-class MockShellOut
-  attr_reader :stdout
-  attr_accessor :valid_exit_codes
-
-  def initialize(stdout)
-    @stdout = stdout
-  end
-
-  def run_command
-  end
-
-  def error!
-  end
-end
+require 'mock_shell_out'
 
 describe Cassandra::Utils::Stats::Cleanup do
   before do
---
+++ test/mock_shell_out.rb
@@ -0,0 +1,14 @@
+class MockShellOut
+  attr_reader :stdout
+  attr_accessor :valid_exit_codes
+
+  def initialize(stdout)
+    @stdout = stdout
+  end
+
+  def run_command
+  end
+
+  def error!
+  end
+end
