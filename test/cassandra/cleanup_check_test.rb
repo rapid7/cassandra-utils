f8aa5e3d3dfac8ed9e3938ef416dfcd9db1a10d5
Frank Mitchell <me@frankmitchell.org>
2016-11-30 11:50:16 -0500

    Provide a failing test case for when cleanup's not running.

--- test/cassandra/cleanup_check_test.rb
+++ test/cassandra/cleanup_check_test.rb
@@ -38,4 +38,21 @@ describe Cassandra::Utils::Stats::Cleanup do
       @cleanup.run!.must_equal true
     end
   end
+
+  it 'fails if cleanup is not running' do
+    shellout = lambda do |command, options|
+      command.must_equal 'nodetool compactionstats'
+
+      results = <<-END
+      pending tasks: 0
+      Active compaction remaining time : n/a
+      END
+
+      MockShellOut.new(results)
+    end
+
+    Mixlib::ShellOut.stub :new, shellout do
+      @cleanup.run!.must_equal false
+    end
+  end
 end
