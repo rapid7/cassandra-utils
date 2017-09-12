2506d60a7fde32e73ab96463338ce13bf9b28c43
Jennifer <jennifer_patrick@rapid7.com>
2017-09-12 15:07:15 -0400

    removed superfluous old code from health.rb

--- lib/cassandra/utils/stats/health.rb
+++ lib/cassandra/utils/stats/health.rb
@@ -66,26 +66,6 @@ module Cassandra
 
         private
 
-        # Run the "nodetool statusgossip' command and return the output
-        #
-        # @return [String, nil] Output from the "nodetool statusgossip" command
-        #
-        def nodetool_statusgossip
-          @nodetool_statusgossip ||= DaemonRunner::ShellOut.new(command: 'nodetool statusgossip')
-          @nodetool_statusgossip.run!
-          @nodetool_statusgossip.stdout
-        end
-
-        # Run the "nodetool statusthrift' command and return the output
-        #
-        # @return [String, nil] Output from the "nodetool statusthrift" command
-        #
-        def nodetool_statusthrift
-          @nodetool_statusthrift||= DaemonRunner::ShellOut.new(command: 'nodetool statusthrift')
-          @nodetool_statusthrift.run!
-          @nodetool_statusthrift.stdout
-        end
-
         # Run the "nodetool netstats' command and return the output
         #
         # @return [String, nil] Output from the "nodetool netstats" command
