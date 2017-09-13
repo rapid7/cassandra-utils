fc524c0b1390868d662e22e33eb8c2b1cfccc48e
Jennifer <jennifer_patrick@rapid7.com>
2017-09-13 13:18:42 -0400

    added some ends in

--- test/cassandra/health_check_test.rb
+++ test/cassandra/health_check_test.rb
@@ -9,15 +9,12 @@ describe Cassandra::Utils::Stats::Health do
     it 'succeeds if node is NORMAL and thrift and gossip are running' do
       @checker.stub :nodetool_netstats, 'Mode: NORMAL' do
         @checker.stub :nodetool_info, "Gossip state: true\nThrift state: true\n" do  @checker.run!.must_equal true
-            @checker.run!.must_equal true
           end
         end
       end
-
     it 'fails if gossip is down' do
       @checker.stub :nodetool_netstats, 'Mode: NORMAL' do
         @checker.stub :nodetool_info, "Gossip state: false\nThrift state: true\n" do  @checker.run!.must_equal true
-            @checker.run!.must_equal false
           end
         end
       end
@@ -25,11 +22,9 @@ describe Cassandra::Utils::Stats::Health do
     it 'fails if thrift is down' do
       @checker.stub :nodetool_netstats, 'Mode: NORMAL' do
         @checker.stub :nodetool_info, "Gossip state: true\nThrift state: false\n" do  @checker.run!.must_equal true
-            @checker.run!.must_equal false
           end
         end
       end
-
     it 'skips thrift and gossip checks if node is not NORMAL' do
       @checker.stub :nodetool_netstats, 'Mode: JOINING' do
         @checker.run!.must_equal true
